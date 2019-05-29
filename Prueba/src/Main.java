7471f9ce2a355aeb960a6d3e007118ebe7474c37
usu4r10 <jesuschicharro@disroot.org>
2019-05-29 13:34:37 +0200

    added suma method

--- Prueba/src/Main.java
+++ Prueba/src/Main.java
@@ -2,5 +2,10 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World! this is a test");
+
+    }
+
+    int suma(int a, int b) {
+        return a + b;
     }
 }
